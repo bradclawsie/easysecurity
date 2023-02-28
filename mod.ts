c3feba0af1c51adf2a918bd8752b3fe42720799c
Brad Clawsie <brad@b7j0c.org>
2023-02-27 19:41:43 -0600

    begin work on Key

--- mod.ts
+++ mod.ts
@@ -1,8 +1,35 @@
 import { crypto } from "https://deno.land/std@0.178.0/crypto/crypto.ts";
 import { toHashString } from "https://deno.land/std@0.178.0/crypto/to_hash_string.ts";
-import { stringToBytes } from "https://deno.land/x/textras@0.1.2/mod.ts";
+import { stringToBytes } from "https://deno.land/x/textras@0.1.3/mod.ts";
 
+/**
+ * produce the hex-encoded string of the sha256 hash of string s
+ * @param {string} s - the string to hash
+ * @returns {Promise<string>} the hex-encoded sha256 of s
+ */
 const sha256Hex = async (s: string): Promise<string> =>
   toHashString(await crypto.subtle.digest("SHA-256", stringToBytes(s)));
 
-export { sha256Hex };
+/**
+ * re-export crypto.randomUUID in this namespace
+ * @returns {string} a random v4 uuid
+ */
+const randomUUID = (): string => crypto.randomUUID();
+
+class Key {
+  cryptoKey: CryptoKey;
+
+  constructor(cryptoKey: CryptoKey) {
+    this.cryptoKey = cryptoKey;
+  }
+
+  static async makeCryptoKey(): Promise<CryptoKey> {
+    return await crypto.subtle.generateKey(
+      { name: "AES-CBC", length: 128 },
+      true,
+      ["encrypt", "decrypt"],
+    );
+  }
+}
+
+export { Key, randomUUID, sha256Hex };
--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
-import { sha256Hex } from "./mod.ts";
+import { Key, sha256Hex } from "./mod.ts";
 
 Deno.test("sha256Hex", async () => {
   const h = await sha256Hex("hello world");
@@ -8,4 +8,10 @@ Deno.test("sha256Hex", async () => {
     "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
     "sha256hex",
   );
+});
+
+Deno.test("key", async () => {
+  const k0 = new Key(await Key.makeCryptoKey());
+  const k1 = new Key(await Key.makeCryptoKey());
+  assertEquals(k0, k1);
 });
